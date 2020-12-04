0bacf9e1cd35ad388e653add926564889ebe4b49
Ivan Kang <ivan@potime.com>
2020-12-04 11:12:20 +0800

    Add Responsive form [.table-responsive] to GridView.

--- src/gii/generators/crud/adminlte3/views/index.php
+++ src/gii/generators/crud/adminlte3/views/index.php
@@ -38,7 +38,9 @@ $this->params['breadcrumbs'][] = $this->title;
 <?php if ($generator->indexWidgetType === 'grid'): ?>
         <?= "<?= " ?>GridView::widget([
             'dataProvider' => $dataProvider,
-            <?= !empty($generator->searchModelClass) ? "'filterModel' => \$searchModel,\n            'columns' => [\n" : "'columns' => [\n"; ?>
+            <?= !empty($generator->searchModelClass) ? "'filterModel' => \$searchModel,\n" : ""; ?>
+            'options' => ['class' => 'grid-view table-responsive'],
+            'columns' => [
                 ['class' => 'yii\grid\SerialColumn'],
 
 <?php
