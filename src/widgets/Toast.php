dc0dfed5025328068bb343237b94054fa1f39307
Ivan Kang <ivan@potime.com>
2021-07-20 14:49:59 +0800

    Added Toast widget.

---
+++ src/widgets/Toast.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace potime\adminlte3\widgets;
+
+use Yii;
+use yii\bootstrap4\Widget;
+
+/**
+ * Toast widget renders a message from session flash. All flash messages are displayed
+ * in the sequence they were assigned using setFlash. You can set message as following:
+ *
+ * ```php
+ * Yii::$app->session->setFlash('error', 'This is the message');
+ * Yii::$app->session->setFlash('success', 'This is the message');
+ * Yii::$app->session->setFlash('info', 'This is the message');
+ * ```
+ *
+ * Multiple messages could be set as follows:
+ *
+ * ```php
+ * Yii::$app->session->setFlash('error', ['Error 1', 'Error 2']);
+ * ```
+ */
+class Toast extends Widget
+{
+    /**
+     * 弹窗标题
+     * @var string
+     */
+    public $title = '提示信息';
+
+    /**
+     * 延时关闭（ms）
+     * @var int
+     */
+    public $delay = 5000;
+
+    /**
+     * 图标 与 样式配置
+     */
+    public $types = [
+        'error' => [
+            'class' => 'toast-error',
+            'icon' => '<i class="icon fas fa-ban mr-2"></i>',
+        ],
+        'danger' => [
+            'class' => 'toast-error',
+            'icon' => '<i class="icon fas fa-ban mr-2"></i>',
+        ],
+        'success' => [
+            'class' => 'toast-success',
+            'icon' => '<i class="icon fas fa-check mr-2"></i>',
+        ],
+        'info' => [
+            'class' => 'toast-info',
+            'icon' => '<i class="icon fas fa-info mr-2"></i>',
+        ],
+        'warning' => [
+            'class' => 'toast-warning',
+            'icon' => '<i class="icon fas fa-exclamation-triangle mr-2"></i>',
+        ],
+        'light' => [
+            'class' => 'toast-success',
+            'icon' => '',
+        ],
+        'dark' => [
+            'class' => 'toast-info',
+            'icon' => '',
+        ]
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function run()
+    {
+        $session = Yii::$app->session;
+        $flashes = $session->getAllFlashes();
+        $appendClass = isset($this->options['class']) ? ' ' . $this->options['class'] : '';
+
+        echo '<div id="toast-container" class="toasts-bottom-right">';
+        foreach ($flashes as $type => $flash) {
+            if (!isset($this->types[$type])) {
+                continue;
+            }
+
+            foreach ((array)$flash as $message) {
+                echo \yii\bootstrap4\Toast::widget([
+                    'options' => [
+                        'data-delay' => $this->delay,
+                        'class' => 'shadow ' . $this->types[$type]['class'] ?? 'toast-info' . $appendClass,
+                    ],
+                    'clientOptions' => 'show',
+                    'title' => ($this->types[$type]['icon'] ?? '') . $this->title,
+                    'body' => $message,
+                ]);
+            }
+            $session->removeFlash($type);
+        }
+        echo '</div>';
+    }
+}
